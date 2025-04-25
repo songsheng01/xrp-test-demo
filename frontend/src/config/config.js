ab48f7cea168abcbf0a3c87def446361b0dbd817
Phao98 <phao@berkeley.edu>
2025-04-24 22:40:05 -0700

    update for ask and bids

---
+++ frontend/src/config/config.js
@@ -0,0 +1,9 @@
+const backendEndpointProduct = "http://localhost:5001";
+const frontendEndpointProduct = "http://localhost:3000";
+
+const config = {
+  BACKEND_ENDPOINT: backendEndpointProduct,
+  FRONTEND_ENDPOINT: frontendEndpointProduct
+};
+
+export default config;
--- frontend/src/pages/TradingPage/TradingPage.jsx
+++ frontend/src/pages/TradingPage/TradingPage.jsx
@@ -1,5 +1,5 @@
 // src/pages/TradingPage.jsx
-import React from "react"
+import React,{ useState,useEffect } from "react"
 import { useParams } from "react-router-dom"
 import TokenHeader from "../../components/Trading/TokenHeader"
 import TokenImage from "../../components/Trading/TokenImage"
@@ -10,12 +10,14 @@ import OrderForm from "../../components/Trading/OrderForm"
 import OrderHistory from "../../components/Trading/OrderHistory"
 import TopBar from "../../components/TopBar/TopBar"
 import SideBar from "../../components/SideBar/SideBar"
-
 import bulbasaurImage from "../../assets/p1.png";
+import config from "../../config/config"
+import axios from "axios";
+
 
 export default function TradingPage() {
   const { tokenId } = useParams()
-  const config = {
+  const config_t = {
     tokenId: tokenId,
     title: "2023 Japanese Pokémon 151 #2 Bulbasaur",
     grade: "0 / 10",
@@ -27,6 +29,24 @@ export default function TradingPage() {
     supply: 100000,
     volume24h: 3500000
   }
+
+  // const comInfo = useState(null);
+  const [asks,setAsk] = useState([]);
+  const [bids,setBid] = useState([]);
+  const fectchInfo = async () => {
+    try {
+      const responese = await axios.post(`${config.BACKEND_ENDPOINT}/api/offers`,{currency:"TESTHPS"}); // NEED TO CHANGE LATTER
+      console.log(responese.data.currentOffer.buyOffers);
+      setAsk(responese.data.currentOffer.sellOffers);
+      setBid(responese.data.currentOffer.buyOffers);
+    }catch(err){
+      console.log(err);
+      console.error('Error refreshing file list:', err);
+    }
+  }
+  useEffect(()=>{
+    fectchInfo();
+  },[]);
 
   return (
     <div className="flex flex-col h-screen bg-gradient-to-br from-gray-100 via-gray-50 to-gray-200 text-gray-900 px-4 pb-2 pt-1">
@@ -45,15 +65,15 @@ export default function TradingPage() {
           <div className="w-2/5 flex flex-col p-2 h-full min-h-0">
             {/* Token header */}
             <TokenHeader
-              title={config.title}
-              grade={config.grade}
+              title={config_t.title}
+              grade={config_t.grade}
               tokenId={tokenId}
             />
             {/* Main image fills rest */}
             <div className="flex-1 min-h-0 flex pt-7">
               <TokenImage
-                imageUrl={config.imageUrl}
-                alt={`${config.title} art`}
+                imageUrl={config_t.imageUrl}
+                alt={`${config_t.title} art`}
               />
             </div>
           </div>
@@ -62,12 +82,12 @@ export default function TradingPage() {
           <div className="flex-1 flex flex-col space-y-4 overflow-y-auto">
             <TokenStats
               tokenId={tokenId}
-              price={config.price}
-              usdPrice={config.usdPrice}
-              change24h={config.change24h}
-              marketCap={config.marketCap}
-              supply={config.supply}
-              volume24h={config.volume24h}
+              price={config_t.price}
+              usdPrice={config_t.usdPrice}
+              change24h={config_t.change24h}
+              marketCap={config_t.marketCap}
+              supply={config_t.supply}
+              volume24h={config_t.volume24h}
             />
 
             <div className="flex gap-4 flex-[0_0_40%] min-h-0">
